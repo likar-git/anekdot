7521c8705bcadf3588b502979a070902be0c4896
likar-git <kod24713@mail.ru>
2021-12-05 19:51:53 +0300

    add thermoprint

--- Anekdot/main.py
+++ Anekdot/main.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import subprocess
+lpr =  subprocess.Popen("/dev/usb/lp0", stdin=subprocess.PIPE)
 website = 'https://www.anekdot.ru/random/anekdot/'
 result = requests.get(website)
 content = result.text
@@ -8,3 +10,4 @@ box = soup.find('div', class_='content content-min')
 transcript = box.find('div', class_='text')
 transcript = transcript.get_text('\n'*2, strip=True)
 print(transcript, '\n' * 4, 't')
+lpr.stdin.write(transcript)
