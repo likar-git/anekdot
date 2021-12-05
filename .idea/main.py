7ab26ae3a39840d1ad93831e101579e5b8e79764
likar-git <kod24713@mail.ru>
2021-12-05 18:54:53 +0300

    work in 1line

--- .idea/main.py
+++ .idea/main.py
@@ -1,1 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
+website = 'https://www.anekdot.ru/id/1272918/'
+result = requests.get(website)
+content = result.text
+soup = BeautifulSoup(content, 'lxml')
+box = soup.find('div', class_='content content-min')
+transcript = box.find('div', class_='text')
+transcript = transcript.get_text()
+print(transcript)
