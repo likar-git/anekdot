79270c0209a0698818fc26548470c3db5d0598c5
likar-git <kod24713@mail.ru>
2021-12-05 19:17:14 +0300

    it works!

--- Anekdot/main.py
+++ Anekdot/main.py
@@ -6,5 +6,5 @@ content = result.text
 soup = BeautifulSoup(content, 'lxml')
 box = soup.find('div', class_='content content-min')
 transcript = box.find('div', class_='text')
-transcript = transcript.get_text('\n', strip=True)
-print(transcript)
+transcript = transcript.get_text('\n'*2, strip=True)
+print(transcript, '\n' * 4, 't')
