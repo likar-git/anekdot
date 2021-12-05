8f44211264b3091b6c8b4b420dcad66a1e4ca1d2
likar-git <kod24713@mail.ru>
2021-12-05 19:17:14 +0300

    it works!

--- Anekdot/main.py
+++ Anekdot/main.py
@@ -1,13 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-import subprocess
-lpr =  subprocess.Popen("/dev/usb/lp0", stdin=subprocess.PIPE)
 website = 'https://www.anekdot.ru/random/anekdot/'
 result = requests.get(website)
 content = result.text
 soup = BeautifulSoup(content, 'lxml')
 box = soup.find('div', class_='content content-min')
 transcript = box.find('div', class_='text')
-transcript = transcript.get_text('\n'*2, strip=True)
-print(transcript, '\n' * 4, 't')
-lpr.stdin.write(transcript)
+transcript = transcript.get_text('\n', strip=True)
+print(transcript)
