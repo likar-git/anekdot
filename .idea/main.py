b2643fb231b5c07b2a350682e41f58e521db1f04
likar-git <kod24713@mail.ru>
2021-12-05 19:07:55 +0300

    it works!

--- .idea/main.py
+++ .idea/main.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-website = 'https://www.anekdot.ru/id/1272918/'
+website = 'https://www.anekdot.ru/random/anekdot/'
 result = requests.get(website)
 content = result.text
 soup = BeautifulSoup(content, 'lxml')
 box = soup.find('div', class_='content content-min')
 transcript = box.find('div', class_='text')
-transcript = transcript.get_text()
+transcript = transcript.get_text('\n', strip=True)
 print(transcript)
