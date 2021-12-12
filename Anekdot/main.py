06218b56c376304f040756e25107291ab3ee805c
likar-git <kod24713@mail.ru>
2021-12-12 19:10:59 +0300

    add button to rasberry

--- Anekdot/main.py
+++ Anekdot/main.py
@@ -1,10 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
-website = 'https://www.anekdot.ru/random/anekdot/'
-result = requests.get(website)
-content = result.text
-soup = BeautifulSoup(content, 'lxml')
-box = soup.find('div', class_='content content-min')
-transcript = box.find('div', class_='text')
-transcript = transcript.get_text('\n'*2, strip=True)
-print(transcript, '\n' * 4)
+import  RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18, GPIO.IN,pull_up_down=GPIO.PUD_UP)
+while True:
+    inputValue = GPIO.input(18)
+    if (inputValue == False):
+        website = 'https://www.anekdot.ru/random/anekdot/'
+        result = requests.get(website)
+        content = result.text
+        soup = BeautifulSoup(content, 'lxml')
+        box = soup.find('div', class_='content content-min')
+        transcript = box.find('div', class_='text')
+        transcript = transcript.get_text('\n'*2, strip=True)
+        print(transcript, '\n' * 4)
+    time.sleep(0.3)
