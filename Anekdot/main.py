eb64d2832516eb1daa884a5a44d0add17fd31e22
likar-git <kod24713@mail.ru>
2021-12-12 19:32:35 +0300

    add stdout

--- Anekdot/main.py
+++ Anekdot/main.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import  RPi.GPIO as GPIO
 import time
-
+import sys
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(18, GPIO.IN,pull_up_down=GPIO.PUD_UP)
 while True:
@@ -15,5 +15,7 @@ while True:
         box = soup.find('div', class_='content content-min')
         transcript = box.find('div', class_='text')
         transcript = transcript.get_text('\n'*2, strip=True)
+        sys.stdout = open('/dev/usb/lp0', 'w')
         print(transcript, '\n' * 4)
+        sys.stdout.close()
     time.sleep(0.3)
